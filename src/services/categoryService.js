b72e2c3d7a401247f1a2474d1739b5ed1a18c5e8
fatimahsnn <fatimahsn9@gmail.com>
2025-02-11 12:48:51 +0500

    fixed images path

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -2,25 +2,25 @@
 
 export const getfeaturedCategories=()=>{
     return[
-        { id: 0, imgpath: "/images/categories/mobilePhones.jpg", name: "Mobile Phones" },
-        { id: 1, imgpath: "/images/categories/computers.jpg", name: "Computer & Acessories" },
-        { id: 2, imgpath: "/images/categories/audioDevices.jpg", name:  "Audio Devices" },
-        { id: 3, imgpath: "/images/categories/wearables.jpg", name: "Wearables" },
-        { id: 4, imgpath: "/images/categories/gamingGears.jpg", name: "Gaming Gears" },
-        { id: 5, imgpath: "/images/categories/smartHomeDevices.png", name: "Smart Home Devices" },
+        { id: 0, imgpath: "./images/categories/mobilePhones.jpg", name: "Mobile Phones" },
+        { id: 1, imgpath: "./images/categories/computers.jpg", name: "Computer & Acessories" },
+        { id: 2, imgpath: "./images/categories/audioDevices.jpg", name:  "Audio Devices" },
+        { id: 3, imgpath: "./images/categories/wearables.jpg", name: "Wearables" },
+        { id: 4, imgpath: "./images/categories/gamingGears.jpg", name: "Gaming Gears" },
+        { id: 5, imgpath: "./images/categories/smartHomeDevices.png", name: "Smart Home Devices" },
       ];
 }
 
 export const getCategories=()=>{
     return[
-        { id: 0, imgpath: "/images/categories/mobilePhones.jpg", name: "Mobile Devices" },
-        { id: 1, imgpath: "/images/categories/computers.jpg", name: "Computers & Accessories" },
-        { id: 2, imgpath: "/images/categories/audioDevices.jpg", name: "Audio Devices" },
-        { id: 3, imgpath: "/images/categories/wearables.jpg", name: "Wearables" },
-        { id: 4, imgpath: "/images/categories/gamingGears.jpg", name: "Gaming Gear" },
-        { id: 5, imgpath: "/images/categories/smartHomeDevices.png", name: "Smart Home Devices" },
-        { id: 6, imgpath: "/images/categories/cameras.jpg", name: "Cameras & Photography" },
-        { id: 7, imgpath: "/images/categories/networkingDevices.jpg", name: "Networking Devices" },
-        { id: 8, imgpath: "/images/categories/accessories.jpg", name: "Accessories" },
+        { id: 0, imgpath: "./images/categories/mobilePhones.jpg", name: "Mobile Devices" },
+        { id: 1, imgpath: "./images/categories/computers.jpg", name: "Computers & Accessories" },
+        { id: 2, imgpath: "./images/categories/audioDevices.jpg", name: "Audio Devices" },
+        { id: 3, imgpath: "./images/categories/wearables.jpg", name: "Wearables" },
+        { id: 4, imgpath: "./images/categories/gamingGears.jpg", name: "Gaming Gear" },
+        { id: 5, imgpath: "./images/categories/smartHomeDevices.png", name: "Smart Home Devices" },
+        { id: 6, imgpath: "./images/categories/cameras.jpg", name: "Cameras & Photography" },
+        { id: 7, imgpath: "./images/categories/networkingDevices.jpg", name: "Networking Devices" },
+        { id: 8, imgpath: "./images/categories/accessories.jpg", name: "Accessories" },
       ];
 }
